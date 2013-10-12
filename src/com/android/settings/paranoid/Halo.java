37d1ed1103f99a965f5637b4b9e6eba1bbd61c9f
Zips <hcolmenares@gmail.com>
2013-10-12 01:59:43 -0400

    Revert "Halo: Add master switch to disable Halo (1/2)"
    Change-Id: I19cf5d2310156032ee55b71ef77a5c5627026854

--- src/com/android/settings/paranoid/Halo.java
+++ src/com/android/settings/paranoid/Halo.java
@@ -50,7 +50,6 @@ import net.margaritov.preference.colorpicker.ColorPickerPreference;
 public class Halo extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_HALO_ENABLED = "halo_enabled";
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_NINJA = "halo_ninja";
@@ -74,7 +73,6 @@ public class Halo extends SettingsPreferenceFragment
     private CheckBoxPreference mHaloNinja;
     private CheckBoxPreference mHaloMsgBox;
     private CheckBoxPreference mHaloUnlockPing;
-    private SwitchPreference mHaloEnabled;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
     private CheckBoxPreference mHaloPause;
@@ -100,11 +98,6 @@ public class Halo extends SettingsPreferenceFragment
 
         mNotificationManager = INotificationManager.Stub.asInterface(
                 ServiceManager.getService(Context.NOTIFICATION_SERVICE));
-
-        mHaloEnabled = (SwitchPreference) findPreference(KEY_HALO_ENABLED);
-        mHaloEnabled.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_ENABLED, 0) == 1);
-        mHaloEnabled.setOnPreferenceChangeListener(this);
 
         mHaloState = (ListPreference)  prefSet.findPreference(KEY_HALO_STATE);
         mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
@@ -225,12 +218,7 @@ public class Halo extends SettingsPreferenceFragment
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-       if (preference == mHaloEnabled) {
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.HALO_ENABLED,
-                    (Boolean) newValue ? 1 : 0);
-            return true;
-        } else if (preference == mHaloState) {
+        if (preference == mHaloState) {
             boolean state = Integer.valueOf((String) newValue) == 1;
             try {
                 mNotificationManager.setHaloPolicyBlack(state);
