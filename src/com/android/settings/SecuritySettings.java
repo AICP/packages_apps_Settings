c78203c990929778787f48c153b20356d428d942
Lars Greiss <kufikugel@googlemail.com>
2013-10-18 15:17:14 +0200

    Settings: ehance Privacy Guard Manager
    move privacy by default option into manager
    fit AOSP guidelines
    add visual inidicator for Privacy Guard off
    
    Change-Id: I50dd2b7ef48c7c949153dde9590a31417fcf0f23

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -85,7 +85,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_NOTIFICATION_ACCESS = "manage_notification_access";
     private static final String PACKAGE_MIME_TYPE = "application/vnd.android.package-archive";
     private static final String KEY_SMS_SECURITY_CHECK_PREF = "sms_security_check_limit";
-    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
     private static final String KEY_APP_SECURITY_CATEGORY = "app_security";
 
     private PackageManager mPM;
@@ -114,8 +113,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private Preference mNotificationAccess;
 
     private boolean mIsPrimary;
-
-    private CheckBoxPreference mPrivacyGuardDefault;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -332,14 +329,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 PreferenceGroup appCategory = (PreferenceGroup)
                         root.findPreference(KEY_APP_SECURITY_CATEGORY);
                 appCategory.removePreference(mSmsSecurityCheck);
-            }
-
-            mPrivacyGuardDefault = (CheckBoxPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
-            try {
-                mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
-                        Settings.Secure.PRIVACY_GUARD_DEFAULT) == 1);
-            } catch (SettingNotFoundException e) {
-                mPrivacyGuardDefault.setChecked(false);
             }
 
         mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
@@ -608,9 +597,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
         } else if (KEY_TOGGLE_VERIFY_APPLICATIONS.equals(key)) {
             Settings.Global.putInt(getContentResolver(), Settings.Global.PACKAGE_VERIFIER_ENABLE,
                     mToggleVerifyApps.isChecked() ? 1 : 0);
-        } else if (KEY_PRIVACY_GUARD_DEFAULT.equals(key)) {
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.PRIVACY_GUARD_DEFAULT,
-                    mPrivacyGuardDefault.isChecked() ? 1 : 0);
         } else {
             // If we didn't handle it, let preferences handle it.
             return super.onPreferenceTreeClick(preferenceScreen, preference);
