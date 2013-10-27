a4f13178a8daed67b5df0bc6d5018fed181944e7
kaiyiz <kaiyiz@codeaurora.org>
2013-10-27 15:37:21 +0100

    Settings: Add preference header to make the RoamingSettings.
    Add preference header to make the RoamingSettings.
    
    CRs-fixed: 525245
    
    Change-Id: I3fe4edd3d76ce09b0b85aebc9228ea9bad6ec9b7

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -51,6 +51,7 @@ import android.os.Bundle;
 import android.os.INetworkManagementService;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.Preference;
@@ -92,6 +93,8 @@ public class Settings extends PreferenceActivity
         "com.android.settings.PARENT_FRAGMENT_TITLE";
     private static final String META_DATA_KEY_PARENT_FRAGMENT_CLASS =
         "com.android.settings.PARENT_FRAGMENT_CLASS";
+
+    private static final String GLOBAL_PROP = "persist.env.phone.global";
 
     private static final String EXTRA_UI_OPTIONS = "settings:ui_options";
 
@@ -482,6 +485,10 @@ public class Settings extends PreferenceActivity
             } else if (id == R.id.multi_sim_settings) {
                 if (!MSimTelephonyManager.getDefault().isMultiSimEnabled())
                     target.remove(header);
+            } else if (id == R.id.global_roaming_settings) {
+                if (!SystemProperties.getBoolean(GLOBAL_PROP, false)) {
+                    target.remove(header);
+                }
             }
 
             if (i < target.size() && target.get(i) == header
