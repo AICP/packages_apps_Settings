49e0dbfcfadd0d4b2f5d7cb89dbcfd0f0cfe35ca
Christopher R. Palmer <crpalmer@gmail.com>
2014-05-09 15:54:28 -0400

    Themes: List ThemeSettings as a valid fragment
    This fixes a force close in Settings in tablet mode.
    
    Change-Id: I6542feba0d0fa8491ab9e092a90709d1034ffca8

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -366,7 +366,8 @@ public class Settings extends PreferenceActivity
         PaymentSettings.class.getName(),
         KeyboardLayoutPickerFragment.class.getName(),
         BlacklistSettings.class.getName(),
-        ApnSettings.class.getName()
+        ApnSettings.class.getName(),
+        ThemeSettings.class.getName()
     };
 
     @Override
