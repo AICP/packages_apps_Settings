47e923f18c86db6e65db26ae781397ed3945ed5a
Sungmin Choi <sungmin.choi@lge.com>
2014-06-09 14:55:22 +0900

    Update preferred network mode message.
    Change-Id: I4718674ff73116cc6bbec4540c3450ed14663c0d

--- src/com/android/settings/RadioInfo.java
+++ src/com/android/settings/RadioInfo.java
@@ -1081,14 +1081,14 @@ public class RadioInfo extends Activity {
             "WCDMA preferred",
             "GSM only",
             "WCDMA only",
-            "GSM auto (PRL)",
+            "GSM/WCDMA auto (PRL)",
             "CDMA auto (PRL)",
             "CDMA only",
             "EvDo only",
-            "GSM/CDMA auto (PRL)",
+            "GSM/WCDMA/CDMA auto (PRL)",
             "LTE/CDMA auto (PRL)",
-            "LTE/GSM auto (PRL)",
-            "LTE/GSM/CDMA auto (PRL)",
+            "LTE/GSM/WCDMA auto (PRL)",
+            "LTE/GSM/WCDMA/CDMA auto (PRL)",
             "LTE only",
             "Unknown"};
 
