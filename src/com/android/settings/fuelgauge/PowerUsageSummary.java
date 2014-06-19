d879bfa4bb08f930e34c635a1c2e56d0cfa3c404
Jorge Ruesga <jorge@ruesga.com>
2014-06-19 11:34:22 -0500

    Reset battery stats [2/2]
    Change-Id: I297df7c6fcc76f1ff01256b506ab63d4f227ece3

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -21,6 +21,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.BatteryStats;
 import android.os.Bundle;
 import android.os.Handler;
@@ -65,7 +66,8 @@ public class PowerUsageSummary extends PreferenceFragment implements
 
     private static final int MENU_STATS_TYPE = Menu.FIRST;
     private static final int MENU_STATS_REFRESH = Menu.FIRST + 1;
-    private static final int MENU_HELP = Menu.FIRST + 2;
+    private static final int MENU_STATS_RESET = Menu.FIRST + 2;
+    private static final int MENU_HELP = Menu.FIRST + 3;
 
     private PreferenceGroup mAppListGroup;
     private Preference mBatteryStatusPref;
@@ -78,6 +80,7 @@ public class PowerUsageSummary extends PreferenceFragment implements
     private static final int MIN_POWER_THRESHOLD = 5;
     private static final int MAX_ITEMS_TO_LIST = 10;
 
+    private BatteryManager mBatteryService;
     private BatteryStatsHelper mStatsHelper;
 
     private BroadcastReceiver mBatteryInfoReceiver = new BroadcastReceiver() {
@@ -108,6 +111,7 @@ public class PowerUsageSummary extends PreferenceFragment implements
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         mStatsHelper.create(icicle);
+        mBatteryService = (BatteryManager) getActivity().getSystemService(Context.BATTERY_SERVICE);
 
         addPreferencesFromResource(R.xml.power_usage_summary);
         mAppListGroup = (PreferenceGroup) findPreference(KEY_APP_LIST);
@@ -198,6 +202,11 @@ public class PowerUsageSummary extends PreferenceFragment implements
                 .setAlphabeticShortcut('r');
         refresh.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
                 MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        MenuItem reset = menu.add(0, MENU_STATS_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_menu_delete_holo_dark)
+                .setAlphabeticShortcut('d');
+        reset.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
+                MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 
         String helpUrl;
         if (!TextUtils.isEmpty(helpUrl = getResources().getString(R.string.help_url_battery))) {
@@ -218,6 +227,11 @@ public class PowerUsageSummary extends PreferenceFragment implements
                 refreshStats();
                 return true;
             case MENU_STATS_REFRESH:
+                mStatsHelper.clearStats();
+                refreshStats();
+                return true;
+            case MENU_STATS_RESET:
+                mStatsHelper.resetStatistics();
                 mStatsHelper.clearStats();
                 refreshStats();
                 return true;
