e2f10c89b6e57bd3be476cba801e47713956ca3e
Tianrui Guo <tianrui@pac-rom.com>
2014-06-24 17:12:33 +0200

    White-ify tab colors
    Make things consistent in KitKat
    
    Change-Id: I4e03d488471f746a2f878ed7248730eac9d672ea
    Signed-off-by: Tianrui Guo <tianrui@pac-rom.com>

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -100,7 +100,7 @@ public class AppOpsSummary extends Fragment {
         mViewPager.setAdapter(adapter);
         mViewPager.setOnPageChangeListener(adapter);
         PagerTabStrip tabs = (PagerTabStrip) rootView.findViewById(R.id.tabs);
-        tabs.setTabIndicatorColorResource(android.R.color.holo_blue_light);
+        tabs.setTabIndicatorColorResource(android.R.color.white);
         tabs.setVisibility(View.GONE);
 
         // We have to do this now because PreferenceFrameLayout looks at it
