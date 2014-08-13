254f01ee72781f1579aaa382180b605c0ddf6d9c
dankoman <dankoman30@gmail.com>
2014-08-13 15:45:35 +0200

    Settings: Slimrecents background color && transparency [1/2]
    Allow user to specify background color and transparency level
        for the slimrecents panel.
    
    PS: now setting sticks through reboots and TRDS state
            changes. also, add reset to default color option.
    
    PS: when resetting to default, use TRDS (png) resources
            instead of custom color
    
    PS: properly set preference summary for default
            value ("TRDS default")
    
    PS: remove LSN dependency
    
    AICPfy
    
    Change-Id: I8dd2543a3201b36f8c310de0da54f2f91ca474ef

--- src/com/android/settings/aicp/Recents.java
+++ src/com/android/settings/aicp/Recents.java
@@ -16,8 +16,10 @@
 
 package com.android.settings.aicp;
 
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -31,6 +33,9 @@ import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 
 import java.util.ArrayList;
 
@@ -38,6 +43,8 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import com.android.settings.util.Helpers;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class Recents extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, OnPreferenceClickListener {
@@ -51,6 +58,9 @@ public class Recents extends SettingsPreferenceFragment implements
             "recent_panel_expanded_mode";
     private static final String RECENT_PANEL_SHOW_TOPMOST =
             "recent_panel_show_topmost";
+    private static final String RECENT_PANEL_BG_COLOR =
+            "recent_panel_bg_color";
+
     private static final String RAM_CIRCLE = "ram_circle";
     private static final String PREF_RECENTS_SWIPE_FLOATING = "recents_swipe";
 
@@ -62,6 +72,10 @@ public class Recents extends SettingsPreferenceFragment implements
     private CheckBoxPreference mRecentsShowTopmost;
     private ListPreference mRamCircle;
     private CheckBoxPreference mRecentsSwipe;
+    private ColorPickerPreference mRecentPanelBgColor;
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -120,6 +134,21 @@ public class Recents extends SettingsPreferenceFragment implements
         mRecentsShowTopmost = (CheckBoxPreference) prefSet.findPreference(RECENT_PANEL_SHOW_TOPMOST);
         mRecentsShowTopmost.setChecked(enableRecentsShowTopmost);
         mRecentsShowTopmost.setOnPreferenceChangeListener(this);
+
+        // Recent panel background color
+        mRecentPanelBgColor =
+                (ColorPickerPreference) findPreference(RECENT_PANEL_BG_COLOR);
+        mRecentPanelBgColor.setOnPreferenceChangeListener(this);
+        final int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.RECENT_PANEL_BG_COLOR, 0x00ffffff);
+        String hexColor = String.format("#%08x", (0x00ffffff & intColor));
+        if (hexColor.equals("#00ffffff")) {
+            mRecentPanelBgColor.setSummary("TRDS default");
+        } else {
+            mRecentPanelBgColor.setSummary(hexColor);
+        }
+            mRecentPanelBgColor.setNewPreviewColor(intColor);
+            setHasOptionsMenu(true);
 
         // Recents swipe
         mRecentsSwipe = (CheckBoxPreference) prefSet.findPreference(PREF_RECENTS_SWIPE_FLOATING);
@@ -182,6 +211,18 @@ public class Recents extends SettingsPreferenceFragment implements
             Settings.System.putInt(resolver,
                     Settings.System.RECENT_PANEL_SHOW_TOPMOST,
                     ((Boolean) objValue) ? 1 : 0);
+        } else if (preference == mRecentPanelBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+            Integer.valueOf(String.valueOf(objValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary("TRDS default");
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.RECENT_PANEL_BG_COLOR,
+                    intHex);
         }
 
         return true;
@@ -191,4 +232,43 @@ public class Recents extends SettingsPreferenceFragment implements
     public boolean onPreferenceClick(Preference preference) {
         return false;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset_default_message)
+                .setIcon(R.drawable.ic_settings_backup)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                resetToDefault();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void resetToDefault() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.reset);
+        alertDialog.setMessage(R.string.qs_style_reset_message);
+        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                resetValues();
+            }
+        });
+        alertDialog.setNegativeButton(R.string.cancel, null);
+        alertDialog.create().show();
+    }
+
+    private void resetValues() {
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.RECENT_PANEL_BG_COLOR, DEFAULT_BACKGROUND_COLOR);
+        mRecentPanelBgColor.setNewPreviewColor(DEFAULT_BACKGROUND_COLOR);
+        mRecentPanelBgColor.setSummary("TRDS default");
+    }
+
 }
