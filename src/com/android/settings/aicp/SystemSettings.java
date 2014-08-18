316521ebf4aa7c8560c9cd53ec1978e9919e09da
Alex-Cruz <mazdarider23@gmail.com>
2014-08-18 16:54:12 +0200

    Toggle Boot Audio on/off
    Credit to Roman/AOKP Backup
    
    AICPfy
    
    Change-Id: I24c8234d71a5198d259f018e0019297180343bbf

--- src/com/android/settings/aicp/SystemSettings.java
+++ src/com/android/settings/aicp/SystemSettings.java
@@ -52,13 +52,15 @@ import java.util.ArrayList;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.util.CMDProcessor;
 import com.android.settings.util.Helpers;
 import com.android.settings.vanir.AppMultiSelectListPreference;
 import com.android.settings.widget.SeekBarPreferenceCham;
 
+import java.io.File;
+import java.lang.Thread;
 import java.util.HashSet;
 import java.util.Set;
-import java.lang.Thread;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -80,6 +82,7 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     private static final String KEY_TRIGGER_TOP = "trigger_top";
     private static final String KEY_TRIGGER_BOTTOM = "trigger_bottom";
     private static final String PREF_PROXIMITY_ON_WAKE = "proximity_on_wake";
+    private static final String DISABLE_BOOTAUDIO = "disable_bootaudio";
 
     private AppMultiSelectListPreference mIncludedAppCircleBar;
 
@@ -90,6 +93,7 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mDontShowNavbar;
     private CheckBoxPreference mNavigationMenuForce;
     private CheckBoxPreference mProximityWake;
+    private CheckBoxPreference mDisableBootAudio;
 
     private ListPreference mNavigationMenu;
 
@@ -190,6 +194,19 @@ public class SystemSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(), Settings.System.PROXIMITY_ON_WAKE, 1);
         }
 
+        // Boot audio
+        mDisableBootAudio = (CheckBoxPreference) findPreference("disable_bootaudio");
+
+        if(!new File("/system/media/audio.mp3").exists() &&
+                !new File("/system/media/boot_audio").exists() ) {
+            mDisableBootAudio.setEnabled(false);
+            mDisableBootAudio.setSummary(R.string.disable_bootaudio_summary_disabled);
+        } else {
+            mDisableBootAudio.setChecked(!new File("/system/media/audio.mp3").exists());
+            if (mDisableBootAudio.isChecked())
+                mDisableBootAudio.setSummary(R.string.disable_bootaudio_summary);
+        }
+
     }
 
 
@@ -225,6 +242,20 @@ public class SystemSettings extends SettingsPreferenceFragment implements
             boolean checked = ((CheckBoxPreference)preference).isChecked();
             Settings.System.putInt(resolver,
                     Settings.System.PROXIMITY_ON_WAKE, checked ? 1 : 0);
+        } else if (preference == mDisableBootAudio) {
+            boolean checked = ((CheckBoxPreference) preference).isChecked();
+            if (checked) {
+                Helpers.getMount("rw");
+                CMDProcessor.runSuCommand(
+                        "mv /system/media/audio.mp3 /system/media/boot_audio");
+                Helpers.getMount("ro");
+                preference.setSummary(R.string.disable_bootaudio_summary);
+            } else {
+                Helpers.getMount("rw");
+                CMDProcessor.runSuCommand(
+                        "mv /system/media/boot_audio /system/media/audio.mp3");
+                Helpers.getMount("ro");
+            }
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
