6fcf9a8b7857edd22f4488b512c8eae5278a4cc8
Adam77Root <adam77root@gmail.com>
2014-09-06 15:57:34 +0200

    Remove derp
    Change-Id: If8ac1d9a845ce7e2d8a89ebbfdf70d04998a2dd0

--- src/com/android/settings/deviceinfo/UsbSettings.java
+++ src/com/android/settings/deviceinfo/UsbSettings.java
@@ -75,15 +75,7 @@ public class UsbSettings extends SettingsPreferenceFragment {
         mMtp = (CheckBoxPreference)root.findPreference(KEY_MTP);
         mPtp = (CheckBoxPreference)root.findPreference(KEY_PTP);
         mUms = (CheckBoxPreference)root.findPreference(KEY_MASS_STORAGE);
-
-        boolean allowMassStorage = false;
-        for (StorageVolume storageVolume : storageVolumes) {
-            if (storageVolume.allowMassStorage()) {
-                allowMassStorage = true;
-                break;
-            }
-        }
-        if (!allowMassStorage) {
+        if (!storageVolumes[0].allowMassStorage()) {
             root.removePreference(mUms);
         }
 
