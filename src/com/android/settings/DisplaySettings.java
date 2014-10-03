72ad09e15b69f47895aea7eff49d421562f72182
Josue Rivera <prbassplayer@gmail.com>
2014-10-03 04:21:41 +0200

    Make smart cover options opt in.
    Thanks to Kit and Griffin for the help
    
    Change-Id: Id8ec763f926a076cbed423cc6614246ae2caf74c
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -91,6 +91,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private ListPreference mScreenTimeoutPreference;
     private Preference mScreenSaverPreference;
 
+    private int[] mSmartCoverCoords;
+
     private ContentObserver mAccelerometerRotationObserver =
             new ContentObserver(new Handler()) {
         @Override
@@ -164,12 +166,25 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 updateBatteryPulseDescription();
             }
 
+        mSmartCoverCoords = getResources().getIntArray(
+                com.android.internal.R.array.config_smartCoverWindowCoords);
+        if(mSmartCoverCoords.length != 4) {
+            // make sure there are exactly 4 dimensions provided, or ignore the values
+            mSmartCoverCoords = null;
+        }
+
         // Disable smart cover
          mDisableSmartCover = (CheckBoxPreference) findPreference(KEY_DISABLE_SMART_COVER);
+         PreferenceCategory wakeupOptions
+                = (PreferenceCategory) findPreference("category_wakeup_options");
          if (mDisableSmartCover != null) {
-             mDisableSmartCover.setChecked(Settings.System.getInt(resolver,
-                     Settings.System.DISABLE_SMART_COVER, 0) == 1);
-             mDisableSmartCover.setOnPreferenceChangeListener(this);
+             if (mSmartCoverCoords == null) {
+                    wakeupOptions.removePreference(mDisableSmartCover);
+             } else {
+                    mDisableSmartCover.setChecked(Settings.System.getInt(resolver,
+                            Settings.System.DISABLE_SMART_COVER, 0) == 1);
+                    mDisableSmartCover.setOnPreferenceChangeListener(this);
+             }
          }
 
             //If we're removed everything, get rid of the category
