59bd74fbb093b1f202d2415b9e1b7b9c077787ac
snak3ater <saeedyasir91@gmail.com>
2015-01-17 21:55:16 +0000

    QS: Longclick action [2/2]
    Long press expanded status bar Settings button opens tiles settings
    
    Thanks to romanbb for status bar weather code & Altaf-Mahdi for Status Bar Header haptic feedback option
    
    Change-Id: I36d6c023e90f47c92f3d77d4a2c138ba15032fc5
    Signed-off-by: snak3ater <saeedyasir91@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -103,5 +103,6 @@ public class Settings extends SettingsActivity {
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AicpSettingsActivity extends SettingsActivity { /* empty */ }
     public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QSTilesSettingsActivity extends SettingsActivity { /* empty */ }
 }
 
