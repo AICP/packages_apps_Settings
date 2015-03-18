990d18ad1b9940958800b01e76c2a13334098da1
BeansTown106 <nbenis106@gmail.com>
2015-03-18 10:58:43 -0400

    Set System Animations to .8 by default (2 of 2)
    Change-Id: I6e2953608b1f36ca15310245ac0b7e31fbe78752

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -2207,7 +2207,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private void writeAnimationScaleOption(int which, AnimationScalePreference pref,
             Object newValue) {
         try {
-            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 1;
+            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 0.8f;
             mWindowManager.setAnimationScale(which, scale);
             updateAnimationScaleValue(which, pref);
         } catch (RemoteException e) {
