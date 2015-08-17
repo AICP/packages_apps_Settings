404e66d62a6dae22d75a56dcd34482c11c7294a3
eyosen <abittin@gmail.com>
2015-08-17 14:43:31 +0300

    aicp: make our features searchable
    Thanks to Shaun Eaton@BlissROM for the idea.
    
    Change-Id: I2fca117c156f0a8158042cf295d5cb97ace2f9b3

--- src/com/android/settings/aicp/RecentsPanelSettings.java
+++ src/com/android/settings/aicp/RecentsPanelSettings.java
@@ -16,9 +16,13 @@
  */
 package com.android.settings.aicp;
 
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.preference.ListPreference;
@@ -26,14 +30,18 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
 import com.android.settings.util.Helpers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecentsPanelSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, Indexable {
 
     private static final String TAG = "RecentsPanelSettings";
 
@@ -126,4 +134,29 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
             mRecentsClearAllLocation.setSummary(res.getString(summary));
         }
     }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.aicp_recents_panel_settings;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    if (Helpers.isPackageInstalled(OMNISWITCH_PACKAGE_NAME, context.getPackageManager())) {
+                        result.add(KEY_OMNISWITCH);
+                    }
+                    return result;
+                }
+            };
 }
