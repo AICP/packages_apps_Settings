4735fc8158518649d4dc6b0cf5039a3cbf533e24
nobe1976 <charlesmc76@gmail.com>
2016-08-01 18:08:01 -0400

    Expose PhoneInfo This is helpful for carrier locked devices to be able to acess certain provisioning items
    PureNexus Edits:
    *hide on wifi only devices
    
    Change-Id: I0f73420c7a7ad1378e8bd8883fa6d69ce563e2ee

--- src/com/android/settings/deviceinfo/Status.java
+++ src/com/android/settings/deviceinfo/Status.java
@@ -56,6 +56,7 @@ public class Status extends SettingsPreferenceFragment {
     private static final String KEY_WIMAX_MAC_ADDRESS = "wimax_mac_address";
     private static final String KEY_SIM_STATUS = "sim_status";
     private static final String KEY_IMEI_INFO = "imei_info";
+    private static final String KEY_RADIO_INFO = "radio_info";
 
     // Broadcasts to listen to for connectivity changes.
     private static final String[] CONNECTIVITY_INTENTS = {
@@ -195,6 +196,7 @@ public class Status extends SettingsPreferenceFragment {
                 || Utils.isWifiOnly(getContext())) {
             removePreferenceFromScreen(KEY_SIM_STATUS);
             removePreferenceFromScreen(KEY_IMEI_INFO);
+            removePreferenceFromScreen(KEY_RADIO_INFO);
         }
     }
 
