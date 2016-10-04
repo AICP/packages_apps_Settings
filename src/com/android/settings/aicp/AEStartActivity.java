8e7b051e10dd346c5c3d53816a9d0a828ff3725f
LorDClockaN <lordclockan@gmail.com>
2016-10-04 21:16:57 +0200

    AE: Start AE from special activity and show summary in dashboard
    PS3: Fix closing AEStartActivity after wxiting AE
    
    Change-Id: I3a5646e7c9461860bf2e699f5152665b46c15297

---
+++ src/com/android/settings/aicp/AEStartActivity.java
@@ -0,0 +1,67 @@
+package com.android.settings.aicp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.Indexable;
+
+public class AEStartActivity extends SettingsPreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.aicp_start_activity);
+
+        startAE();
+    }
+
+    private void startAE(){
+        Intent AicpExtrasIntent = new Intent();
+        AicpExtrasIntent.setClassName("com.lordclockan", "com.lordclockan.aicpextras.MainActivity");
+        startActivity(AicpExtrasIntent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        finish();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
+
+    private static class SummaryProvider implements SummaryLoader.SummaryProvider {
+
+        private final Context mContext;
+        private final SummaryLoader mSummaryLoader;
+
+        public SummaryProvider(Context context, SummaryLoader summaryLoader) {
+            mContext = context;
+            mSummaryLoader = summaryLoader;
+        }
+
+        @Override
+        public void setListening(boolean listening) {
+            if (listening) {
+                mSummaryLoader.setSummary(this, mContext.getString(R.string.summary_aicp));
+            }
+        }
+    }
+
+    public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
+            = new SummaryLoader.SummaryProviderFactory() {
+        @Override
+        public SummaryLoader.SummaryProvider createSummaryProvider(Activity activity,
+                                                                   SummaryLoader summaryLoader) {
+            return new SummaryProvider(activity, summaryLoader);
+        }
+    };
+}
