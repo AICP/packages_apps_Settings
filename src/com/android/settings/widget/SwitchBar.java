ebf984590f8361d67fc5a8bdfcc8f26907636db7
LorDClockaN <lordclockan@gmail.com>
2016-10-26 17:07:57 +0200

    Merge branch 'android-7.1.0_r4' into n7.1
    Change-Id: Iaa035b62bf1b36690fad0a7011661a56b3e5f98f

--- src/com/android/settings/widget/SwitchBar.java
+++ src/com/android/settings/widget/SwitchBar.java
@@ -45,7 +45,7 @@ import java.util.ArrayList;
 public class SwitchBar extends LinearLayout implements CompoundButton.OnCheckedChangeListener,
         View.OnClickListener {
 
-    public static interface OnSwitchChangeListener {
+    public interface OnSwitchChangeListener {
         /**
          * Called when the checked state of the Switch has changed.
          *
@@ -66,7 +66,8 @@ public class SwitchBar extends LinearLayout implements CompoundButton.OnCheckedC
     private int mStateOnLabel = R.string.switch_on_text;
     private int mStateOffLabel = R.string.switch_off_text;
 
-    private boolean mDisabledByAdmin = false;
+    private boolean mLoggingIntialized;
+    private boolean mDisabledByAdmin;
     private EnforcedAdmin mEnforcedAdmin = null;
 
     private String mMetricsTag;
@@ -244,7 +245,6 @@ public class SwitchBar extends LinearLayout implements CompoundButton.OnCheckedC
             RestrictedLockUtils.sendShowAdminSupportDetailsIntent(mContext, mEnforcedAdmin);
         } else {
             final boolean isChecked = !mSwitch.isChecked();
-            MetricsLogger.count(mContext, mMetricsTag + "/switch_bar|" + isChecked, 1);
             setChecked(isChecked);
         }
     }
@@ -258,6 +258,10 @@ public class SwitchBar extends LinearLayout implements CompoundButton.OnCheckedC
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (mLoggingIntialized) {
+            MetricsLogger.count(mContext, mMetricsTag + "/switch_bar|" + isChecked, 1);
+        }
+        mLoggingIntialized = true;
         propagateChecked(isChecked);
     }
 
