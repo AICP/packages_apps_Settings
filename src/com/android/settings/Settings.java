864bd37ef5b30a799e5ab2dd9573448bd403d166
tsubus <tsubus@ilwt.org>
2017-02-02 16:12:32 +0100

    Launch SuperSU from Settings
    Icon from AICP MM
    
    Change-Id: Ia5dafad75501fb9443e8d8b0470a10b51d0aa4d5

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -168,4 +168,5 @@ public class Settings extends SettingsActivity {
     public static class SmartbarSettingsActivity extends SettingsActivity { /* empty */ }
     public static class PulseSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AmbientSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SuperSUActivity extends SettingsActivity { /* empty */ }
 }
