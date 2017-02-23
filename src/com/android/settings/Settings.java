bd7752faf663d89b38f72e34af18e80af8985b92
Alex Cruz <mazdarider23@gmail.com>
2017-02-23 17:54:31 +0100

    Fix Expanded Desktop tile longpress action
    Change-Id: I9aba0a9bf2b211267d5c2a5a22537cc669b179bb

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -170,4 +170,5 @@ public class Settings extends SettingsActivity {
     public static class PulseSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AmbientSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
+    public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
 }
