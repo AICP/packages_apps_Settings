ef5d52968da7452d5b3c4031f2602953c2286e97
SpiritCroc <spiritcroc@gmail.com>
2017-07-31 15:13:58 +0200

    Vibration mode entered indicated by vibrating switch [2/2]
    Change-Id: I2029d7e16aeca98d8b06e874db13429f3bf3994f

--- src/com/android/settings/notification/OtherSoundSettings.java
+++ src/com/android/settings/notification/OtherSoundSettings.java
@@ -72,6 +72,7 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_CHARGING_SOUNDS = "charging_sounds";
     private static final String KEY_DOCKING_SOUNDS = "docking_sounds";
     private static final String KEY_TOUCH_SOUNDS = "touch_sounds";
+        private static final String KEY_VIBRATION_MODE_INDICATION = "vibration_mode_indication";
     private static final String KEY_VIBRATE_ON_TOUCH = "vibrate_on_touch";
     private static final String KEY_DOCK_AUDIO_MEDIA = "dock_audio_media";
     private static final String KEY_EMERGENCY_TONE = "emergency_tone";
@@ -124,6 +125,10 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
             return super.setSetting(context, value);
         }
     };
+
+    private static final SettingPref PREF_VIBRATION_MODE_INDICATION = new SettingPref(
+            TYPE_SYSTEM, KEY_VIBRATION_MODE_INDICATION, System.VIBRATION_MODE_INDICATION,
+            DEFAULT_ON);
 
     private static final SettingPref PREF_VIBRATE_ON_TOUCH = new SettingPref(
             TYPE_SYSTEM, KEY_VIBRATE_ON_TOUCH, System.HAPTIC_FEEDBACK_ENABLED, DEFAULT_ON) {
@@ -184,6 +189,7 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
         PREF_CHARGING_SOUNDS,
         PREF_DOCKING_SOUNDS,
         PREF_TOUCH_SOUNDS,
+        PREF_VIBRATION_MODE_INDICATION,
         PREF_VIBRATE_ON_TOUCH,
         PREF_DOCK_AUDIO_MEDIA,
         PREF_EMERGENCY_TONE,
