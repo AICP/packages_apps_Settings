c6c3a481bcf48f1311d402be537e163e8daf9d44
SpiritCroc <spiritcroc@gmail.com>
2017-08-09 19:05:57 +0200

    ColorEngine: Settings: default to default bg
    Change-Id: If23039c8f163e66a373ae26d09d418f2e562d9f2

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -310,7 +310,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             final int accentColorValue = Settings.Secure.getInt(getContext().getContentResolver(),
                     Settings.Secure.THEME_ACCENT_COLOR, 0);
             final int primaryColorValue = Settings.Secure.getInt(getContext().getContentResolver(),
-                    Settings.Secure.THEME_PRIMARY_COLOR, 2);
+                    Settings.Secure.THEME_PRIMARY_COLOR, 0);
             mThemePreference.setSummary(PreviewSeekBarPreferenceFragment.getInfoText(getContext(),
                     false, accentColorValue, primaryColorValue) + ", " +
                     PreviewSeekBarPreferenceFragment.getInfoText(getContext(), true,
@@ -462,7 +462,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             final int accentColorValue = Settings.Secure.getInt(getContext().getContentResolver(),
                     Settings.Secure.THEME_ACCENT_COLOR, 0);
             final int primaryColorValue = Settings.Secure.getInt(getContext().getContentResolver(),
-                    Settings.Secure.THEME_PRIMARY_COLOR, 2);
+                    Settings.Secure.THEME_PRIMARY_COLOR, 0);
             mThemePreference.setSummary(PreviewSeekBarPreferenceFragment.getInfoText(getContext(),
                     false, accentColorValue, primaryColorValue) + ", " +
                     PreviewSeekBarPreferenceFragment.getInfoText(getContext(), true,
@@ -579,7 +579,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             final int accentColorValue = Settings.Secure.getInt(getContext().getContentResolver(),
                     Settings.Secure.THEME_ACCENT_COLOR, 0);
             final int primaryColorValue = Settings.Secure.getInt(getContext().getContentResolver(),
-                    Settings.Secure.THEME_PRIMARY_COLOR, 2);
+                    Settings.Secure.THEME_PRIMARY_COLOR, 0);
             mThemePreference.setSummary(PreviewSeekBarPreferenceFragment.getInfoText(getContext(),
                     false, accentColorValue, primaryColorValue) + ", " +
                     PreviewSeekBarPreferenceFragment.getInfoText(getContext(), true,
