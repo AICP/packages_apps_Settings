c1e0f5fb3b6848a674901dafbc23f9d382772920
Alex Cruz <du.alexcruz@gmail.com>
2017-10-02 20:20:21 -0400

    Change developer options strings to something less dumb
    In the past we gone ahead and just enabled developer options by default and
    just removed the code used for 'tap to enable'. This was never an issue but
    considering some options like adaptive options are now booby trapped by Google
    with developer options, it doesn't make sense to do that in O and maybe future
    versions. (see Pixel launcher and Launcher3)
    
    With this said, we need better strings. Users need to know that messing with
    developer options is not a joke and could end up slowing their device down or
    making it unusable for them if they're too noobish.
    
    Change-Id: Ie34ab8df5d6cea496099d0f1c4eda19c08d164b4

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -169,7 +169,7 @@ public class BuildNumberPreferenceController extends AbstractPreferenceControlle
                 }
                 mDevHitToast = Toast.makeText(mContext,
                         mContext.getResources().getQuantityString(
-                                R.plurals.show_dev_countdown, mDevHitCountdown,
+                                R.plurals.show_dev_countdown_custom, mDevHitCountdown,
                                 mDevHitCountdown),
                         Toast.LENGTH_SHORT);
                 mDevHitToast.show();
@@ -182,7 +182,7 @@ public class BuildNumberPreferenceController extends AbstractPreferenceControlle
             if (mDevHitToast != null) {
                 mDevHitToast.cancel();
             }
-            mDevHitToast = Toast.makeText(mContext, R.string.show_dev_already,
+            mDevHitToast = Toast.makeText(mContext, R.string.show_dev_already_custom,
                     Toast.LENGTH_LONG);
             mDevHitToast.show();
             mMetricsFeatureProvider.action(
@@ -221,7 +221,7 @@ public class BuildNumberPreferenceController extends AbstractPreferenceControlle
         if (mDevHitToast != null) {
             mDevHitToast.cancel();
         }
-        mDevHitToast = Toast.makeText(mContext, R.string.show_dev_on,
+        mDevHitToast = Toast.makeText(mContext, R.string.show_dev_on_custom,
                 Toast.LENGTH_LONG);
         mDevHitToast.show();
         // This is good time to index the Developer Options
