bdb25b02edb88e92cefaf7be3daab17c12cded0c
Alex Cruz <du.alexcruz@gmail.com>
2017-11-02 14:33:41 -0400

    Add Ambient display category / Preference controllers to handle switches
    This commit reverts the commit below
    
    https://github.com/DirtyUnicorns/android_packages_apps_Settings/commit/7848620666170bd731616db4b683a8eaa2645818#diff-5e2c4e4c5255b6f429e822301573fcef
    
    It also adds the 'Always on' switch to Settings since it was removed
    from the SystemUI Tuner in the commit below
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/04c88687caa2b2b91ea25f9660e4db0ecbd0948a
    
    This commit puts both switches inside a category 'Ambient options' while preserving
    the conditions originally placed on to them by Google. It also continues to give us
    the option of removing the entire category for devices such as the OP3/OP5 and any
    device that uses a custom Doze package.
    
    - DozePreferenceController was used as a template for DozeAlwaysOn and DozeCategory
    
    - AlwaysOn 'isAvailable' code taken from the commit below
    https://github.com/DirtyUnicorns/android_frameworks_base/blob/d7e5a2b886225945b85de365aab1668d558fe0a1/core/java/com/android/internal/hardware/AmbientDisplayConfiguration.java#L82-L85
    
    - Add preference and controller for custom doze package (com.custom.ambient.display)
    
    Change-Id: If25aa1cba5156b02d37b8754fede30bc87742be8

---
+++ src/com/android/settings/display/CustomDozePreferenceController.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+
+
+import com.android.settings.core.PreferenceController;
+
+public class CustomDozePreferenceController extends PreferenceController {
+
+    private static final String KEY_CUSTOM_DOZE = "custom_doze";
+
+    public CustomDozePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_CUSTOM_DOZE;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        return false;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        if (mContext.getApplicationContext().getResources().getBoolean(com.android.internal.R.bool.config_alt_ambient_display)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
