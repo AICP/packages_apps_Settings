22900771fc4b2ea2c6ba0450d32b80ec7657cfe9
maxwen <max.weninger@gmail.com>
2018-02-25 12:18:38 +0100

    [2/2] Settings: add USB paranoia mode
    Change-Id: Iedc1a0e516a260ca6f54bd2f2afe5dcf14b85e6b

--- Android.mk
+++ Android.mk
@@ -38,7 +38,8 @@ LOCAL_JAVA_LIBRARIES := \
 LOCAL_STATIC_JAVA_LIBRARIES := \
     jsr305 \
     settings-logtags \
-    org.lineageos.platform.internal
+    org.lineageos.platform.internal \
+    AicpGear-preference
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
