723e1634bda70479a24bd5c1f33b1057b3332c4e
SpiritCroc <dev@spiritcroc.de>
2018-09-09 11:33:20 +0200

    Settings: AICPfy about device
    Change-Id: I596ec14c4cf0562e8dbf5b28b5bbe15acb404d5c

--- src/com/android/settings/deviceinfo/firmwareversion/AicpVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/AicpVersionPreferenceController.java
@@ -17,18 +17,40 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.preference.Preference;
 
 import com.android.settings.R;
+import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settingslib.RestrictedLockUtils;
+import com.android.settingslib.RestrictedLockUtilsInternal;
 
 public class AicpVersionPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "AicpVersionPreferenceController";
+    private static final String KEY_AICP_VERSION = "aicp_version";
+    private static final int DELAY_TIMER_MILLIS = 500;
+    private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
+
+    private final UserManager mUserManager;
+    private final long[] mAicpHits = new long[ACTIVITY_TRIGGER_COUNT];
+
+    private RestrictedLockUtils.EnforcedAdmin mFunDisallowedAdmin;
+    private boolean mFunDisallowedBySystem;
 
     public AicpVersionPreferenceController(Context context, String key) {
         super(context, key);
+        mUserManager = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
+        initializeAdminPermissions();
     }
 
     @Override
@@ -40,4 +62,49 @@ public class AicpVersionPreferenceController extends BasePreferenceController {
     public CharSequence getSummary() {
         return mContext.getString(R.string.aicp_version_summary);
     }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+        if (Utils.isMonkeyRunning()) {
+            return false;
+        }
+
+        aicpArrayCopy();
+        mAicpHits[mAicpHits.length - 1] = SystemClock.uptimeMillis();
+        if (mAicpHits[0] >= (SystemClock.uptimeMillis() - DELAY_TIMER_MILLIS)) {
+            if (mUserManager.hasUserRestriction(UserManager.DISALLOW_FUN)) {
+                if (mFunDisallowedAdmin != null && !mFunDisallowedBySystem) {
+                    RestrictedLockUtils.sendShowAdminSupportDetailsIntent(mContext,
+                            mFunDisallowedAdmin);
+                }
+                Log.d(TAG, "Sorry, no fun for you!");
+                return false;
+            }
+
+            final Intent intent = new Intent(Intent.ACTION_MAIN)
+                                    .setClassName(
+                                "com.android.systemui", "com.android.systemui.egg.MLandActivity");
+            try {
+                mContext.startActivity(intent);
+            } catch (Exception e) {
+                Log.e(TAG, "Unable to start activity " + intent.toString());
+            }
+        }
+        return true;
+    }
+
+    void aicpArrayCopy() {
+        System.arraycopy(mAicpHits, 1, mAicpHits, 0, mAicpHits.length - 1);
+    }
+
+    void initializeAdminPermissions() {
+        mFunDisallowedAdmin = RestrictedLockUtilsInternal.checkIfRestrictionEnforced(
+                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
+        mFunDisallowedBySystem = RestrictedLockUtilsInternal.hasBaseUserRestriction(
+                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
+    }
+
 }
