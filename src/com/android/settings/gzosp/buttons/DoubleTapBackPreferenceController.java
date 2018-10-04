e90c8075581c740e11022b86816f62e5a6a0bdd6
Chris <chris@cypheros.co>
2018-10-04 12:41:12 -0400

    buttons: Properly initiate button action summaries
    Since our actions can be reconfigured, we must return the default set
    value instead of 0
    
    Change-Id: I10ad4c8a5d0e31bb68dc45099b0ffb7671fff440

--- src/com/android/settings/gzosp/buttons/DoubleTapBackPreferenceController.java
+++ src/com/android/settings/gzosp/buttons/DoubleTapBackPreferenceController.java
@@ -34,6 +34,8 @@ public class DoubleTapBackPreferenceController extends BasePreferenceController
 
     private Context mContext;
     private ListPreference mPref;
+
+    private int mDefaultBehavior;
     private final String mKey;
 
     public DoubleTapBackPreferenceController(Context context, String key) {
@@ -57,9 +59,9 @@ public class DoubleTapBackPreferenceController extends BasePreferenceController
         super.displayPreference(screen);
         mPref = (ListPreference) screen.findPreference(getPreferenceKey());
         if (mPref == null) return;
-        int defaultBehavior = mContext.getResources().getInteger(
+        mDefaultBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_doubleTapOnBackKeyBehavior);
-        int value = Settings.System.getInt(mContext.getContentResolver(), KEY_BACK_DOUBLE_TAP_ACTION, defaultBehavior);
+        int value = Settings.System.getInt(mContext.getContentResolver(), KEY_BACK_DOUBLE_TAP_ACTION, mDefaultBehavior);
         mPref.setValue(Integer.toString(value));
     }
 
@@ -73,7 +75,7 @@ public class DoubleTapBackPreferenceController extends BasePreferenceController
 
     @Override
     public CharSequence getSummary() {
-        int value = Settings.System.getInt(mContext.getContentResolver(), KEY_BACK_DOUBLE_TAP_ACTION, 0);
+        int value = Settings.System.getInt(mContext.getContentResolver(), KEY_BACK_DOUBLE_TAP_ACTION, mDefaultBehavior);
         int index = mPref.findIndexOfValue(Integer.toString(value));
         return mPref.getEntries()[index];
     }
