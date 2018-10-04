3d6be8493c7466bead5b74b49dda39ef7eef8484
Chris <chris@cypheros.co>
2018-10-04 12:49:17 -0400

    buttons: Disable recents actions when QuickStep is enabled
    Swipe up gesture interface does not hold a dedicated recents button,
    therefore a recents action shouldn't be configurable. Disable the pref
    when QuickStep is active
    
    Change-Id: I5ac9140fb59de46d75c28859423d771578d1767b

--- src/com/android/settings/gzosp/buttons/DoubleTapAppSwitchPreferenceController.java
+++ src/com/android/settings/gzosp/buttons/DoubleTapAppSwitchPreferenceController.java
@@ -59,10 +59,13 @@ public class DoubleTapAppSwitchPreferenceController extends BasePreferenceContro
         super.displayPreference(screen);
         mPref = (ListPreference) screen.findPreference(getPreferenceKey());
         if (mPref == null) return;
+        boolean isQuickStep = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0) != 0;
         mDefaultBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_doubleTapOnAppSwitchKeyBehavior);
         int value = Settings.System.getInt(mContext.getContentResolver(), KEY_APP_SWITCH_DOUBLE_TAP_ACTION, mDefaultBehavior);
         mPref.setValue(Integer.toString(value));
+        mPref.setEnabled(isQuickStep ? false : true);
     }
 
     @Override
