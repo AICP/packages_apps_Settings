20dd6db41006072f1326c20d8a14ece29434060f
Shubham Singh <coolsks94@gmail.com>
2018-10-08 20:02:47 +0530

    Settings: Allow users to switch between Pie/Oreo recents [3/3]
    Change-Id: I8cd4ddad62f31f37860b4dfb4137915402d0a2a1
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/gestures/SwipeUpPreferenceController.java
+++ src/com/android/settings/gestures/SwipeUpPreferenceController.java
@@ -46,7 +46,7 @@ public class SwipeUpPreferenceController extends GesturePreferenceController {
     static boolean isGestureAvailable(Context context) {
         boolean hasNav = false;
         final boolean configEnabled =
-                context.getResources().getBoolean(R.bool.config_custom_swipe_up_gesture_setting_available);
+                context.getResources().getBoolean(R.bool.config_custom_swipe_up_gesture_setting_available) || !isPieRecentsEnabled(context);
         try {
             hasNav = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
         } catch (RemoteException ex) {
@@ -65,6 +65,11 @@ public class SwipeUpPreferenceController extends GesturePreferenceController {
             return false;
         }
         return true;
+    }
+
+    static boolean isPieRecentsEnabled(Context mContext) {
+       return Settings.System.getInt(mContext.getContentResolver(),
+                      Settings.System.RECENTS_COMPONENT, 0) == 0;
     }
 
     @Override
