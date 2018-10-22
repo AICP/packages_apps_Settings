e78fdd00086975fd1045464321b7c478d8f71c56
Alex Cruz <alex@dirtyunicorns.com>
2018-10-22 10:19:28 +0200

    Settings: Remove AOSP Ambient display conditionally
    The OnePlus devices we currently support (OP3/T and OP5) both use a alterative
    doze package. Having this under Settings/Display will not only confuse the user
    and possibly cause problems.
    
    - Add preference and controller for custom doze package (com.custom.ambient.display)
    
    - Check for potter to launch it's doze package
    
    If potter is not detected, we launch the alterative one found
    in our other devices.
    
    Change-Id: Id90a503c18fc01d5e3add7264bb9451596c036ac

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -22,6 +22,7 @@ import android.provider.SearchIndexableResource;
 import com.android.internal.hardware.AmbientDisplayConfiguration;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.display.AmbientDisplayCustomPreferenceController;
 import com.android.settings.display.AmbientDisplayPreferenceController;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
@@ -87,6 +88,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new NightDisplayPreferenceController(context));
         controllers.add(new NightModePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
+        controllers.add(new AmbientDisplayCustomPreferenceController(context));
         controllers.add(new AmbientDisplayPreferenceController(
                 context,
                 new AmbientDisplayConfiguration(context),
