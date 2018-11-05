0773e31912f0c2b4ad04d88bb837bb59264445e1
Chris <chris@cypheros.co>
2018-11-05 07:34:41 -0500

    fuelguage: Add battery level animation
    In O-MR1 we reverted the scrapped aosp implementation of this which got
    rid of the header controller. This time around we've reworked the code a
    bit to work in the header controller, since controllers are much more
    efficient
    
    Change-Id: Ibfe3cc50f192281a0bede670acf9ace1d7523f02

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -221,6 +221,13 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         updateBatteryTipFlag(icicle);
     }
 
+    public void onActivityCreated(Bundle outState) {
+        super.onActivityCreated(outState);
+        if (outState != null) {
+            mBatteryHeaderPreferenceController.saveInstanceState(outState);
+        }
+    }
+
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.FUELGAUGE_POWER_USAGE_SUMMARY_V2;
