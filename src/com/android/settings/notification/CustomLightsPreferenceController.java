c7e304ead12c3eae0b00a9633af400d67fbae566
SpiritCroc <dev@spiritcroc.de>
2018-12-11 13:07:22 +0100

    Custom notification light: remember default color [2/2]
    Change-Id: I8d5d8e26fc4e74a6e5c424aa3b4122e3b4bd7b9d

--- src/com/android/settings/notification/CustomLightsPreferenceController.java
+++ src/com/android/settings/notification/CustomLightsPreferenceController.java
@@ -64,6 +64,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
             if (mChannel.getLightColor() != 0) {
                 ColorPickerPreference mCustomLight = (ColorPickerPreference) preference;
                 mCustomLight.setNewPreviewColor(mChannel.getLightColor());
+                mCustomLight.setDefaultValue(mChannel.getUnmodifiedLightColor());
             }
         }
     }
@@ -72,7 +73,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (mChannel != null) {
             mLedColor = ((Integer) newValue).intValue();
-            mChannel.setLightColor(mLedColor);
+            mChannel.setModifiedLightColor(mLedColor);
             saveChannel();
         }
         return true;
