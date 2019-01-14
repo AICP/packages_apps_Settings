b785d12b6b4f28b29c6280b6fc1301ecdd7dd4dd
Steve Kondik <shade@chemlab.org>
2019-01-14 09:18:01 +0100

    Settings: Add a SelfRemovingPreference for device-specific doze settings
     * If a device has its own panel, we should place it under
       display settings instead of having to clutter up the dashboard.
     * This uses the new SelfRemovingPreference API to allow the external
       app to update the view.
     * The action for this is "com.aicp.settings.device.DOZE_SETTINGS".
     * Adapted to 10:
    	- switch to androidx
    	- update AmbientDisplayConfiguration package
    
    Change-Id: I22fd7e6307b370807415ce2132bed4808b2befe1

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -18,10 +18,12 @@ package com.android.settings;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
 
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.display.AmbientDisplayPreferenceController;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.DarkUIPreferenceController;
@@ -49,6 +51,7 @@ public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
 
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
+    private static final String KEY_AMBIENT_DISPLAY = "doze_device_settings";
 
     @Override
     public int getMetricsCategory() {
@@ -90,6 +93,10 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new NightDisplayPreferenceController(context));
         controllers.add(new NightModePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
+        controllers.add(new AmbientDisplayPreferenceController(
+                context,
+                new AmbientDisplayConfiguration(context),
+                KEY_AMBIENT_DISPLAY));
         controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
