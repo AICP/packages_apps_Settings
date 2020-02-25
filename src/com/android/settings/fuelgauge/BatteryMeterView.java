8f1ef601b5e11e4c4601d27b54499901baf68020
Ali B <abittin@gmail.com>
2020-02-25 08:58:40 +0300

    Battery: add missing style override
    We have set battery style in settings to q-style portrait
    but have not informed our themed battery class. So, it wasnt
    being properly drawn.
    
    Change-Id: Ifd2428cc2881905982f02864bca7b8356078ffbe

--- src/com/android/settings/fuelgauge/BatteryMeterView.java
+++ src/com/android/settings/fuelgauge/BatteryMeterView.java
@@ -129,5 +129,10 @@ public class BatteryMeterView extends ImageView {
         public int getIntrinsicHeight() {
             return mIntrinsicHeight;
         }
+
+        @Override
+        public int getMeterStyle() {
+            return BATTERY_STYLE_Q;
+        }
     }
 }
