99da3ec204f96c4bd119610d56493d74295d3a4b
AshutoshSundresh <62063875+AshutoshSundresh@users>
2020-07-31 06:30:25 +0000

    BatteryHealth: Make it optional
    * Allows a device maintainer to show/hide the BatteryHealth preference in Power Settings.
    * Helps certain OnePlus devices with unreliable sys nodes
    * Disabled by default
    
    @neobuddy89: Clean up implementation.
    
    AICP Edits: [TEMP]: Don't add to search index for now.
    
    Change-Id: Iea321d199187a09706a68e22178ff6626ffef105
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -122,6 +122,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @VisibleForTesting
     Preference mBatteryUsagePreference;
 
+    boolean mBatteryHealthSupported;
+
     @VisibleForTesting
     final ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
         @Override
@@ -208,6 +210,13 @@ public class PowerUsageSummary extends PowerUsageBase implements
         mBatteryChargeCycles = (PowerGaugePreference) findPreference(
                 KEY_BATTERY_CHARGE_CYCLES);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
+
+        mBatteryHealthSupported = getResources().getBoolean(R.bool.config_supportBatteryHealth);
+        if (!mBatteryHealthSupported) {
+            getPreferenceScreen().removePreference(mCurrentBatteryCapacity);
+            getPreferenceScreen().removePreference(mDesignedBatteryCapacity);
+            getPreferenceScreen().removePreference(mBatteryChargeCycles);
+        }
 
         if (Utils.isBatteryPresent(getContext())) {
             restartBatteryInfoLoader();
@@ -299,9 +308,12 @@ public class PowerUsageSummary extends PowerUsageBase implements
         restartBatteryInfoLoader();
 
         mBatteryTempPref.setSummary(BatteryInfo.batteryTemp + " \u2103");
-        mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(getResources().getString(R.string.config_batteryCalculatedCapacity)));
-        mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(getResources().getString(R.string.config_batteryDesignCapacity)));
-        mBatteryChargeCycles.setSubtitle(parseBatteryCycle(getResources().getString(R.string.config_batteryChargeCycles)));
+
+        if (mBatteryHealthSupported) {
+            mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(getResources().getString(R.string.config_batteryCalculatedCapacity)));
+            mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(getResources().getString(R.string.config_batteryDesignCapacity)));
+            mBatteryChargeCycles.setSubtitle(parseBatteryCycle(getResources().getString(R.string.config_batteryChargeCycles)));
+        }
     }
 
     @VisibleForTesting
@@ -440,6 +452,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(R.xml.power_usage_summary) {
+
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
@@ -447,5 +460,16 @@ public class PowerUsageSummary extends PowerUsageBase implements
                         keys.add(KEY_OPTIMIZED_CHARGE);
                     return keys;
                 }
+
+                /**@Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    if (!context.getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
+                        keys.add(KEY_CURRENT_BATTERY_CAPACITY);
+                        keys.add(KEY_DESIGNED_BATTERY_CAPACITY);
+                        keys.add(KEY_BATTERY_CHARGE_CYCLES);
+                    return keys;
+                    }
+                }**/
             };
 }
