ezio84/abc-n_packages_apps_Settings

Hide notification light color preference if no multicolor led
on and off time will be still available instead

src/com/android/settings/notification/CustomLightsPreferenceController.java
@@ -55,7 +55,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
             return false;
         }
         return checkCanBeVisible(NotificationManager.IMPORTANCE_DEFAULT)
-                && canPulseLight();
+                && canPulseLight() && isMultiColorLed();
     }
 
     public void updateState(Preference preference) {
@@ -88,6 +88,11 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
         }
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.NOTIFICATION_LIGHT_PULSE, 1) == 1;
+    }
+
+    boolean isMultiColorLed() {
+        return mContext.getResources()
+                .getBoolean(com.android.internal.R.bool.config_multiColorBatteryLed);
     }
 
     private void showLedPreview() {

AICP/packages_apps_Settings

Change-Id: If7552628a9bd42643e9b9d50f74cf8da8faf3077

src/com/android/settings/notification/app/CustomLightsPreferenceController.java
@@ -57,7 +57,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
             return false;
         }
         return checkCanBeVisible(NotificationManager.IMPORTANCE_DEFAULT)
-                && canPulseLight();
+                && canPulseLight() && isMultiColorLed();
     }
 
     public void updateState(Preference preference) {
@@ -90,6 +90,11 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
         }
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.NOTIFICATION_LIGHT_PULSE, 1) == 1;
+    }
+
+    boolean isMultiColorLed() {
+        return mContext.getResources()
+                .getBoolean(com.android.internal.R.bool.config_intrusiveNotificationLed);
     }
 
     private void showLedPreview() {