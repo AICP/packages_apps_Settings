syberia-project/platform_packages_apps_Settings

Settings: Fix Device Config check for Ramping ringer
Vibrator service checks for Ramping ringer enabled in device config
https://github.com/syberia-project/platform_frameworks_base/commit/164202a9f5a2047886f2ee4fcf853bb0de8ad121

Remove this menu entry when it's not enabled too

Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: If92e9b304e586798994259c4ed8b0ff3ee56c7cd

src/com/android/settings/sound/VibrateForCallsPreferenceController.java
@@ -33,8 +33,12 @@ public class VibrateForCallsPreferenceController extends BasePreferenceControlle
 
     private static final int ON = 1;
     private static final int OFF = 0;
+
     @VisibleForTesting
     static final String RAMPING_RINGER_ENABLED = "ramping_ringer_enabled";
+
+    private static final boolean mRampingEnabled = DeviceConfig.getBoolean(
+                DeviceConfig.NAMESPACE_TELEPHONY, RAMPING_RINGER_ENABLED, false);
 
     public VibrateForCallsPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -43,15 +47,14 @@ public class VibrateForCallsPreferenceController extends BasePreferenceControlle
     @Override
     @AvailabilityStatus
     public int getAvailabilityStatus() {
-        return Utils.isVoiceCapable(mContext) && !DeviceConfig.getBoolean(
-                DeviceConfig.NAMESPACE_TELEPHONY, RAMPING_RINGER_ENABLED, false)
+        return Utils.isVoiceCapable(mContext)
             ? AVAILABLE
             : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
     public CharSequence getSummary() {
-        if (Settings.Global.getInt(
+        if (mRampingEnabled && Settings.Global.getInt(
                 mContext.getContentResolver(),
                 Settings.Global.APPLY_RAMPING_RINGER, OFF) == ON) {
             return mContext.getText(R.string.vibrate_when_ringing_option_ramping_ringer);

AICP/packages_apps_Settings

src/com/android/settings/sound/VibrateForCallsPreferenceController.java
@@ -33,8 +33,12 @@ public class VibrateForCallsPreferenceController extends BasePreferenceControlle
 
     private static final int ON = 1;
     private static final int OFF = 0;
+
     @VisibleForTesting
     static final String RAMPING_RINGER_ENABLED = "ramping_ringer_enabled";
+
+    private static final boolean isRampingEnabled = DeviceConfig.getBoolean(
+                DeviceConfig.NAMESPACE_TELEPHONY, RAMPING_RINGER_ENABLED, false);
 
     public VibrateForCallsPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -43,15 +47,14 @@ public class VibrateForCallsPreferenceController extends BasePreferenceControlle
     @Override
     @AvailabilityStatus
     public int getAvailabilityStatus() {
-        return Utils.isVoiceCapable(mContext) && !DeviceConfig.getBoolean(
-                DeviceConfig.NAMESPACE_TELEPHONY, RAMPING_RINGER_ENABLED, false)
+        return Utils.isVoiceCapable(mContext)
             ? AVAILABLE
             : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
     public CharSequence getSummary() {
-        if (Settings.Global.getInt(
+        if (isRampingEnabled && Settings.Global.getInt(
                 mContext.getContentResolver(),
                 Settings.Global.APPLY_RAMPING_RINGER, OFF) == ON) {
             return mContext.getText(R.string.vibrate_when_ringing_option_ramping_ringer);