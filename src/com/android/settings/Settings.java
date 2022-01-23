e3f69ef036584d0551cadd977ce6e570e3e3b559
LorDClockaN <lordclockan@gmail.com>
2022-01-23 19:13:01 +0530

    Settings: Launch AicpExtras from Settings
    Adapted to Android 15
    
    Change-Id: I0af9cea462c02fb6f17d95076aacce08d7f4f10c
    Signed-off-by: LorDClockaN  <lordclockan@gmail.com>
    Signed-off-by: maxwen <max.weninger@gmail.com>
    Signed-off-by: doc HD <doc.divxm@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -538,4 +538,7 @@ public class Settings extends SettingsActivity {
     public static class ContentProtectionSettingsActivity extends SettingsActivity { /* empty */ }
     public static class MagnificationActivity extends SettingsActivity { /* empty */ }
     public static class FlashNotificationsActivity extends SettingsActivity { /* empty */ }
+
+    // AICP Activities
+    public static class StartAeActivity extends SettingsActivity {}
 }
