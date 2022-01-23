cc011dd3dfac77d50fdef50a44dcaed2a1b6c056
LorDClockaN <lordclockan@gmail.com>
2022-01-23 19:13:01 +0530

    Settings: Launch AicpExtras from Settings
    Adapted to Android 15
    
    Change-Id: I0af9cea462c02fb6f17d95076aacce08d7f4f10c
    Signed-off-by: LorDClockaN  <lordclockan@gmail.com>
    Signed-off-by: maxwen <max.weninger@gmail.com>
    Signed-off-by: doc HD <doc.divxm@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -44,6 +44,7 @@ import com.android.settings.accounts.AccountDashboardFragment;
 import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.accounts.ChooseAccountFragment;
 import com.android.settings.accounts.ManagedProfileSettings;
+import com.android.settings.aicp.StartAe;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.AppStorageSettings;
 import com.android.settings.applications.ProcessStatsSummary;
@@ -432,6 +433,7 @@ public class SettingsGateway {
             SupervisionDashboardFragment.class.getName(),
             SummarizationPreferenceFragment.class.getName(),
             SafetyCenterFragment.class.getName(),
+            StartAe.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -480,5 +482,6 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.ShadeSettingsActivity.class.getName(),
+            Settings.StartAeActivity.class.getName(),
     };
 }
