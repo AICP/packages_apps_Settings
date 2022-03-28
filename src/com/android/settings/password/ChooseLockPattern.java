LineageOS/android_packages_apps_Settings

Settings: Make FRP go through ChooseLockPatternSize if needed
Change-Id: I70b0ddb20033dd5505c391763a0a58fd2f9c8004

src/com/android/settings/password/ChooseLockPattern.java
@@ -104,6 +104,7 @@ public class ChooseLockPattern extends SettingsActivity {
 
         public IntentBuilder(Context context) {
             mIntent = new Intent(context, ChooseLockPatternSize.class);
+            mIntent.putExtra("destination", ChooseLockPattern.class.getName());
             mIntent.putExtra(EncryptionInterstitial.EXTRA_REQUIRE_PASSWORD, false);
             mIntent.putExtra(ChooseLockGeneric.CONFIRM_CREDENTIALS, false);
             mIntent.putExtra(ChooseLockSettingsHelper.EXTRA_KEY_HAS_CHALLENGE, false);

src/com/android/settings/password/ChooseLockPatternSize.java
@@ -95,8 +95,10 @@ public class ChooseLockPatternSize extends SettingsActivity {
                 patternSize = 3;
             }
 
-            Intent intent = new Intent(getActivity(), ChooseLockPattern.class);
-            intent.putExtras(getActivity().getIntent().getExtras());
+            Bundle extras = getActivity().getIntent().getExtras();
+            Intent intent = new Intent();
+            intent.setClassName(getActivity(), extras.getString("destination"));
+            intent.putExtras(extras);
             intent.putExtra("pattern_size", patternSize);
             intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
             startActivity(intent);

src/com/android/settings/password/ChooseLockSettingsHelper.java
@@ -377,6 +377,9 @@ public final class ChooseLockSettingsHelper {
 
     public boolean launchFrpConfirmationActivity(int request, @Nullable CharSequence header,
             @Nullable CharSequence description, @Nullable CharSequence alternateButton) {
+        Bundle extras = new Bundle();
+        extras.putString("destination", ConfirmLockPattern.class.getName());
+
         return launchConfirmationActivity(
                 request /* request */,
                 null /* title */,
@@ -388,7 +391,7 @@ public final class ChooseLockSettingsHelper {
                 0 /* challenge */,
                 LockPatternUtils.USER_FRP /* userId */,
                 alternateButton /* alternateButton */,
-                null /* extras */,
+                extras /* extras */,
                 false /* foregroundOnly */);
     }
 
@@ -403,7 +406,9 @@ public final class ChooseLockSettingsHelper {
         switch (mLockPatternUtils.getKeyguardStoredPasswordQuality(effectiveUserId)) {
             case DevicePolicyManager.PASSWORD_QUALITY_SOMETHING:
                 launched = launchConfirmationActivity(request, title, header, description,
-                        returnCredentials || hasChallenge
+                        extras != null && userId == LockPatternUtils.USER_FRP
+                                ? ChooseLockPatternSize.class
+                                : returnCredentials || hasChallenge
                                 ? ConfirmLockPattern.InternalActivity.class
                                 : ConfirmLockPattern.class, returnCredentials, external,
                                 hasChallenge, challenge, userId, alternateButton, extras,

AICP/packages_apps_Settings

src/com/android/settings/password/ChooseLockPattern.java
@@ -103,6 +103,7 @@ public class ChooseLockPattern extends SettingsActivity {
 
         public IntentBuilder(Context context) {
             mIntent = new Intent(context, ChooseLockPatternSize.class);
+            mIntent.putExtra("className", ChooseLockPattern.class.getName());
             mIntent.putExtra(EncryptionInterstitial.EXTRA_REQUIRE_PASSWORD, false);
             mIntent.putExtra(ChooseLockGeneric.CONFIRM_CREDENTIALS, false);
         }

src/com/android/settings/password/ChooseLockPatternSize.java
@@ -95,8 +95,10 @@ public class ChooseLockPatternSize extends SettingsActivity {
                 patternSize = 3;
             }
 
-            Intent intent = new Intent(getActivity(), ChooseLockPattern.class);
-            intent.putExtras(getActivity().getIntent().getExtras());
+            Bundle extras = getActivity().getIntent().getExtras();
+            Intent intent = new Intent();
+            intent.setClassName(getActivity(), extras.getString("className"));
+            intent.putExtras(extras);
             intent.putExtra("pattern_size", patternSize);
             intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
             startActivity(intent);

src/com/android/settings/password/ChooseLockSettingsHelper.java
@@ -314,7 +314,9 @@ public final class ChooseLockSettingsHelper {
         switch (mLockPatternUtils.getKeyguardStoredPasswordQuality(effectiveUserId)) {
             case DevicePolicyManager.PASSWORD_QUALITY_SOMETHING:
                 launched = launchConfirmationActivity(request, title, header, description,
-                        returnCredentials || forceVerifyPath
+                        userId == LockPatternUtils.USER_FRP
+                                ? ChooseLockPatternSize.class
+                                : returnCredentials || forceVerifyPath
                                 ? ConfirmLockPattern.InternalActivity.class
                                 : ConfirmLockPattern.class, returnCredentials, external,
                                 forceVerifyPath, userId, alternateButton, allowAnyUser,
@@ -363,6 +365,9 @@ public final class ChooseLockSettingsHelper {
         intent.setClassName(SETTINGS_PACKAGE_NAME, activityClass.getName());
         intent.putExtra(SettingsBaseActivity.EXTRA_PAGE_TRANSITION_TYPE,
                 SettingsTransitionHelper.TransitionType.TRANSITION_SLIDE);
+        if (userId == LockPatternUtils.USER_FRP) {
+            intent.putExtra("className", ConfirmLockPattern.class.getName());
+        }
 
         Intent inIntent = mFragment != null ? mFragment.getActivity().getIntent() :
                 mActivity.getIntent();