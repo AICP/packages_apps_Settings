3c81f6adb36d319535a9657e3c8c4614ed3e0728
Julian Veit <Claymore1298@gmail.com>
2025-05-19 16:25:08 +0200

    Settings: switch to AICP version
    adapt for w16.1
    
    Change-Id: I1fd635c0677d24611d3476d743272c110d26efd7

--- src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreference.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreference.kt
@@ -44,7 +44,7 @@ class LineageVersionDetailPreference :
         get() = "lineage_version"
 
     override val title: Int
-        get() = org.lineageos.platform.internal.R.string.lineage_version
+        get() = R.string.aicp_version
 
     override val indexable
         get() = false
