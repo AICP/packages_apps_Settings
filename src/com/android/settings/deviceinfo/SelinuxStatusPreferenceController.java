c8914020b163e4e4a71f4469558e1bcd323c9ead
Julian Veit <claymore1298@gmail.com>
2026-01-07 05:25:42 +0000

    fixup! Settings: Add back SELinux info
    Change-Id: I0af9cea462c02fb6f17d95076aacce08d7f4f09a

--- src/com/android/settings/deviceinfo/SelinuxStatusPreferenceController.java
+++ src/com/android/settings/deviceinfo/SelinuxStatusPreferenceController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.deviceinfo.firmwareversion;
+package com.android.settings.deviceinfo;
 
 import android.content.Context;
 import android.os.SELinux;
