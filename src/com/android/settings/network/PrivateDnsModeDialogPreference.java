899351e56f25ac9a06cf27ec722d00af0526e71a
Tad <tad@spotco.us>
2026-01-23 09:10:49 +0100

    Add more 'Private DNS' options
    This adds fifteen DNS providers as available presets.
    
    Credit: CalyxOS
    - Chirayu Desai <chirayudesai1@gmail.com>
      https://review.calyxos.org/c/CalyxOS/platform_packages_apps_Settings/+/17176
    
    Change-Id: I3beee821ed12902a35c0d68ca13a57ad39039e8b

--- src/com/android/settings/network/PrivateDnsModeDialogPreference.java
+++ src/com/android/settings/network/PrivateDnsModeDialogPreference.java
@@ -73,12 +73,40 @@ public class PrivateDnsModeDialogPreference extends CustomDialogPreferenceCompat
     private static final Map<Integer, Integer> PRIVATE_DNS_MAP;
 
     // Only used in Settings, update on additions to ConnectivitySettingsUtils
-    private static final int PRIVATE_DNS_MODE_CLOUDFLARE = 4;
+    private static final int PRIVATE_DNS_MODE_ADGUARD = 4;
+    private static final int PRIVATE_DNS_MODE_APPLIEDPRIVACY = 5;
+    private static final int PRIVATE_DNS_MODE_CIRA = 6;
+    private static final int PRIVATE_DNS_MODE_CLEANBROWSING = 7;
+    private static final int PRIVATE_DNS_MODE_CLOUDFLARE = 8;
+    private static final int PRIVATE_DNS_MODE_CONTROLD = 9;
+    private static final int PRIVATE_DNS_MODE_CZNIC = 10;
+    private static final int PRIVATE_DNS_MODE_DNSZERO = 11;
+    private static final int PRIVATE_DNS_MODE_GOOGLE = 12;
+    private static final int PRIVATE_DNS_MODE_MULLVAD = 13;
+    private static final int PRIVATE_DNS_MODE_QUADNINE = 14;
+    private static final int PRIVATE_DNS_MODE_RESTENA = 15;
+    private static final int PRIVATE_DNS_MODE_SWITCH = 16;
+    private static final int PRIVATE_DNS_MODE_UNCENSOREDDNS = 17;
+    private static final int PRIVATE_DNS_MODE_DNSSB = 18;
 
     static {
         PRIVATE_DNS_MAP = new HashMap<>();
         PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_OFF, R.id.private_dns_mode_off);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_ADGUARD, R.id.private_dns_mode_adguard);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_APPLIEDPRIVACY, R.id.private_dns_mode_appliedprivacy);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_CIRA, R.id.private_dns_mode_cira);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_CLEANBROWSING, R.id.private_dns_mode_cleanbrowsing);
         PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_CLOUDFLARE, R.id.private_dns_mode_cloudflare);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_CONTROLD, R.id.private_dns_mode_controld);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_CZNIC, R.id.private_dns_mode_cznic);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_DNSZERO, R.id.private_dns_mode_dnszero);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_DNSSB, R.id.private_dns_mode_dnssb);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_GOOGLE, R.id.private_dns_mode_google);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_MULLVAD, R.id.private_dns_mode_mullvad);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_QUADNINE, R.id.private_dns_mode_quadnine);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_RESTENA, R.id.private_dns_mode_restena);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_SWITCH, R.id.private_dns_mode_switch);
+        PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_UNCENSOREDDNS, R.id.private_dns_mode_uncensoreddns);
         PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_OPPORTUNISTIC, R.id.private_dns_mode_opportunistic);
         PRIVATE_DNS_MAP.put(PRIVATE_DNS_MODE_PROVIDER_HOSTNAME, R.id.private_dns_mode_provider);
     }
@@ -144,10 +172,36 @@ public class PrivateDnsModeDialogPreference extends CustomDialogPreferenceCompat
         if (mMode == PRIVATE_DNS_MODE_PROVIDER_HOSTNAME) {
             final String privateDnsHostname =
                     ConnectivitySettingsManager.getPrivateDnsHostname(context);
-            final String cloudflareHostname =
-                    context.getString(R.string.private_dns_hostname_cloudflare);
-            if (privateDnsHostname.equals(cloudflareHostname)) {
+            if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_adguard))) {
+                mMode = PRIVATE_DNS_MODE_ADGUARD;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_appliedprivacy))) {
+                mMode = PRIVATE_DNS_MODE_APPLIEDPRIVACY;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_cira))) {
+                mMode = PRIVATE_DNS_MODE_CIRA;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_cleanbrowsing))) {
+                mMode = PRIVATE_DNS_MODE_CLEANBROWSING;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_cloudflare))) {
                 mMode = PRIVATE_DNS_MODE_CLOUDFLARE;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_controld))) {
+                mMode = PRIVATE_DNS_MODE_CONTROLD;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_cznic))) {
+                mMode = PRIVATE_DNS_MODE_CZNIC;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_dnszero))) {
+                mMode = PRIVATE_DNS_MODE_DNSZERO;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_dnssb))) {
+                mMode = PRIVATE_DNS_MODE_DNSSB;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_google))) {
+                mMode = PRIVATE_DNS_MODE_GOOGLE;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_mullvad))) {
+                mMode = PRIVATE_DNS_MODE_MULLVAD;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_quadnine))) {
+                mMode = PRIVATE_DNS_MODE_QUADNINE;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_restena))) {
+                mMode = PRIVATE_DNS_MODE_RESTENA;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_switch))) {
+                mMode = PRIVATE_DNS_MODE_SWITCH;
+            } else if (privateDnsHostname.equals(context.getString(R.string.private_dns_hostname_uncensoreddns))) {
+                mMode = PRIVATE_DNS_MODE_UNCENSOREDDNS;
             }
         }
         mRadioGroup = view.findViewById(R.id.private_dns_radio_group);
@@ -157,9 +211,51 @@ public class PrivateDnsModeDialogPreference extends CustomDialogPreferenceCompat
         // Initial radio button text
         final RadioButton offRadioButton = view.findViewById(R.id.private_dns_mode_off);
         offRadioButton.setText(com.android.settingslib.R.string.private_dns_mode_off);
+        final RadioButton adguardRadioButton =
+                view.findViewById(R.id.private_dns_mode_adguard);
+        adguardRadioButton.setText(R.string.private_dns_mode_adguard);
+        final RadioButton appliedprivacyRadioButton =
+                view.findViewById(R.id.private_dns_mode_appliedprivacy);
+        appliedprivacyRadioButton.setText(R.string.private_dns_mode_appliedprivacy);
+        final RadioButton ciraRadioButton =
+                view.findViewById(R.id.private_dns_mode_cira);
+        ciraRadioButton.setText(R.string.private_dns_mode_cira);
+        final RadioButton cleanbrowsingRadioButton =
+                view.findViewById(R.id.private_dns_mode_cleanbrowsing);
+        cleanbrowsingRadioButton.setText(R.string.private_dns_mode_cleanbrowsing);
         final RadioButton cloudflareRadioButton =
                 view.findViewById(R.id.private_dns_mode_cloudflare);
         cloudflareRadioButton.setText(R.string.private_dns_mode_cloudflare);
+        final RadioButton controldRadioButton =
+                view.findViewById(R.id.private_dns_mode_controld);
+        controldRadioButton.setText(R.string.private_dns_mode_controld);
+        final RadioButton cznicRadioButton =
+                view.findViewById(R.id.private_dns_mode_cznic);
+        cznicRadioButton.setText(R.string.private_dns_mode_cznic);
+        final RadioButton dnszeroRadioButton =
+                view.findViewById(R.id.private_dns_mode_dnszero);
+        dnszeroRadioButton.setText(R.string.private_dns_mode_dnszero);
+        final RadioButton dnssbRadioButton =
+                view.findViewById(R.id.private_dns_mode_dnssb);
+        dnssbRadioButton.setText(R.string.private_dns_mode_dnssb);
+        final RadioButton googleRadioButton =
+                view.findViewById(R.id.private_dns_mode_google);
+        googleRadioButton.setText(R.string.private_dns_mode_google);
+        final RadioButton mullvadRadioButton =
+                view.findViewById(R.id.private_dns_mode_mullvad);
+        mullvadRadioButton.setText(R.string.private_dns_mode_mullvad);
+        final RadioButton quadnineRadioButton =
+                view.findViewById(R.id.private_dns_mode_quadnine);
+        quadnineRadioButton.setText(R.string.private_dns_mode_quadnine);
+        final RadioButton restenaRadioButton =
+                view.findViewById(R.id.private_dns_mode_restena);
+        restenaRadioButton.setText(R.string.private_dns_mode_restena);
+        final RadioButton switchRadioButton =
+                view.findViewById(R.id.private_dns_mode_switch);
+        switchRadioButton.setText(R.string.private_dns_mode_switch);
+        final RadioButton uncensoreddnsRadioButton =
+                view.findViewById(R.id.private_dns_mode_uncensoreddns);
+        uncensoreddnsRadioButton.setText(R.string.private_dns_mode_uncensoreddns);
         final RadioButton opportunisticRadioButton =
                 view.findViewById(R.id.private_dns_mode_opportunistic);
         opportunisticRadioButton.setText(
@@ -195,8 +291,36 @@ public class PrivateDnsModeDialogPreference extends CustomDialogPreferenceCompat
     public void onCheckedChanged(RadioGroup group, int checkedId) {
         if (checkedId == R.id.private_dns_mode_off) {
             mMode = PRIVATE_DNS_MODE_OFF;
+        } else if (checkedId == R.id.private_dns_mode_adguard) {
+            mMode = PRIVATE_DNS_MODE_ADGUARD;
+        } else if (checkedId == R.id.private_dns_mode_appliedprivacy) {
+            mMode = PRIVATE_DNS_MODE_APPLIEDPRIVACY;
+        } else if (checkedId == R.id.private_dns_mode_cira) {
+            mMode = PRIVATE_DNS_MODE_CIRA;
+        } else if (checkedId == R.id.private_dns_mode_cleanbrowsing) {
+            mMode = PRIVATE_DNS_MODE_CLEANBROWSING;
         } else if (checkedId == R.id.private_dns_mode_cloudflare) {
             mMode = PRIVATE_DNS_MODE_CLOUDFLARE;
+        } else if (checkedId == R.id.private_dns_mode_controld) {
+            mMode = PRIVATE_DNS_MODE_CONTROLD;
+        } else if (checkedId == R.id.private_dns_mode_cznic) {
+            mMode = PRIVATE_DNS_MODE_CZNIC;
+        } else if (checkedId == R.id.private_dns_mode_dnszero) {
+            mMode = PRIVATE_DNS_MODE_DNSZERO;
+        } else if (checkedId == R.id.private_dns_mode_dnssb) {
+            mMode = PRIVATE_DNS_MODE_DNSSB;
+        } else if (checkedId == R.id.private_dns_mode_google) {
+            mMode = PRIVATE_DNS_MODE_GOOGLE;
+        } else if (checkedId == R.id.private_dns_mode_mullvad) {
+            mMode = PRIVATE_DNS_MODE_MULLVAD;
+        } else if (checkedId == R.id.private_dns_mode_quadnine) {
+            mMode = PRIVATE_DNS_MODE_QUADNINE;
+        } else if (checkedId == R.id.private_dns_mode_restena) {
+            mMode = PRIVATE_DNS_MODE_RESTENA;
+        } else if (checkedId == R.id.private_dns_mode_switch) {
+            mMode = PRIVATE_DNS_MODE_SWITCH;
+        } else if (checkedId == R.id.private_dns_mode_uncensoreddns) {
+            mMode = PRIVATE_DNS_MODE_UNCENSOREDDNS;
         } else if (checkedId == R.id.private_dns_mode_opportunistic) {
             mMode = PRIVATE_DNS_MODE_OPPORTUNISTIC;
         } else if (checkedId == R.id.private_dns_mode_provider) {
@@ -287,10 +411,50 @@ public class PrivateDnsModeDialogPreference extends CustomDialogPreferenceCompat
 
             ConnectivitySettingsManager.setPrivateDnsHostname(context,
                     mHostnameText.getText().toString());
+        } else if (mMode == PRIVATE_DNS_MODE_ADGUARD) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_adguard));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_APPLIEDPRIVACY) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_appliedprivacy));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_CIRA) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_cira));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_CLEANBROWSING) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_cleanbrowsing));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
         } else if (mMode == PRIVATE_DNS_MODE_CLOUDFLARE) {
-            final String cloudflareHostname =
-                    context.getString(R.string.private_dns_hostname_cloudflare);
-            ConnectivitySettingsManager.setPrivateDnsHostname(context, cloudflareHostname);
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_cloudflare));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_CONTROLD) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_controld));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_CZNIC) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_cznic));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_DNSZERO) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_dnszero));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_DNSSB) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_dnssb));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_GOOGLE) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_google));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_MULLVAD) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_mullvad));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_QUADNINE) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_quadnine));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_RESTENA) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_restena));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_SWITCH) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_switch));
+            modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
+        } else if (mMode == PRIVATE_DNS_MODE_UNCENSOREDDNS) {
+            ConnectivitySettingsManager.setPrivateDnsHostname(context, context.getString(R.string.private_dns_hostname_uncensoreddns));
             modeToSet = PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
         }
 
