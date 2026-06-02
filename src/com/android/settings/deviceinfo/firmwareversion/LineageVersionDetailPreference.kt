83374770392a96a979abc82479e4cea400022fb6
mosimchah <mosimchah@gmail.com>
2026-06-02 03:17:59 -0400

    Settings: fixup versioning
    Change-Id: I92c7b6ca9716b8875c76a82b2bbb79e9d1d3473b

--- src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreference.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreference.kt
@@ -102,7 +102,7 @@ class LineageVersionDetailPreference :
         const val ACTIVITY_TRIGGER_COUNT = 3
         const val DELAY_TIMER_MILLIS = 500L
 
-        const val LINEAGE_VERSION_PROPERTY: String = "ro.lineage.version"
+        const val LINEAGE_VERSION_PROPERTY: String = "ro.aicp.version"
 
         const val PLATLOGO_PACKAGE_NAME: String = "org.lineageos.lineageparts"
         const val PLATLOGO_ACTIVITY_CLASS: String = PLATLOGO_PACKAGE_NAME + ".logo.PlatLogoActivity"
--- src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreferenceController.java
@@ -42,7 +42,7 @@ public class LineageVersionDetailPreferenceController extends BasePreferenceCont
     private static final int DELAY_TIMER_MILLIS = 500;
     private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
-    private static final String KEY_LINEAGE_VERSION_PROP = "ro.lineage.version";
+    private static final String KEY_LINEAGE_VERSION_PROP = "ro.aicp.version";
 
     private static final String PLATLOGO_PACKAGE_NAME = "org.lineageos.lineageparts";
     private static final String PLATLOGO_ACTIVITY_CLASS =
